52c65f4152263759c350f955c6e85ac26078d076
anakpayah <afifansyafaqi@ymail.com>
2021-01-10 20:46:13 +0700

    AssetManager edited (afifan0234)

--- Final Project/src/id/ac/its/richard017/syafiq089/afifan234/planetpang/AssetManager.java
+++ Final Project/src/id/ac/its/richard017/syafiq089/afifan234/planetpang/AssetManager.java
@@ -7,12 +7,30 @@ import javax.swing.ImageIcon;
 public class AssetManager {
 	public static Image PLANET_LEFT;
 	public static Image PLANET_RIGHT;
+	
 	public static Image BACKGROUND_SPACE;
 	public static Image BACKGROUND_MENU;
-	public static Image BUTTON_START;
-	public static Image BUTTON_CREDITS;
-	public static Image BUTTON_EXIT;
-	public static Image BUTTON_HIGHSCORE;
+	
+	public static Image BUTTON_START, 
+						START_HOVER, 
+						START_PRESSED;
+
+	public static Image BUTTON_CREDITS, 
+						CREDITS_HOVER, 
+						CREDITS_PRESSED;
+		
+	public static Image BUTTON_EXIT, 
+						EXIT_HOVER, 
+						EXIT_PRESSED;
+	
+	public static Image BUTTON_HIGHSCORE, 
+						HIGHSCORE_HOVER, 
+						HIGHSCORE_PRESSED;
+	
+	public static Image BUTTON_ACHIEVEMENT,
+						ACHIEVEMENT_HOVER,
+						ACHIEVEMENT_PRESSED;
+	
 	public static Image ARROW_UP;
 	public static Image ARROW_DOWN;
 	public static Image ARROW_LEFT;
@@ -26,12 +44,26 @@ public class AssetManager {
 	public static void LoadAssets() {
 		PLANET_LEFT = loadImage("sprite/Planet1.png");
 		PLANET_RIGHT = loadImage("sprite/Planet2.png");
+		
 		BACKGROUND_SPACE = loadImage("sprite/BackgroundSpace.png");
 		BACKGROUND_MENU = loadImage("sprite/MainMenuBg.png");
+		
 		BUTTON_START = loadImage("sprite/Button_Start_Normal.png");
+		START_HOVER = loadImage("sprite/Button_Start_MouseEnter.png");
+		START_PRESSED = loadImage("sprite/Button_Start_MouseClick.png");
+		
 		BUTTON_CREDITS = loadImage("sprite/Button_Credits_Normal.png");
+		CREDITS_HOVER = loadImage("sprite/Button_Start_MouseEnter.png");
+		CREDITS_PRESSED = loadImage("sprite/Button_Start_MouseClick.png");
+		
 		BUTTON_EXIT = loadImage("sprite/Button_Exit_Normal.png");
+		EXIT_HOVER = loadImage("sprite/Button_Start_MouseEnter.png");
+		EXIT_PRESSED = loadImage("sprite/Button_Start_MouseClick.png");
+		
 		BUTTON_HIGHSCORE = loadImage("sprite/Button_Highscore_Normal.png");
+		HIGHSCORE_HOVER = loadImage("sprite/Button_Start_MouseEnter.png");
+		HIGHSCORE_PRESSED = loadImage("sprite/Button_Start_MouseClick.png");
+		
 		ARROW_UP = loadImage("sprite/Arrow_Up.png");
 		ARROW_DOWN = loadImage("sprite/Arrow_Down.png");
 		ARROW_LEFT = loadImage("sprite/Arrow_Left.png");
